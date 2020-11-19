748c14162b8eed699642574c9ee9a8b14e4cc119
Erik Knaake <e.knaake@student.han.nl>
2020-11-19 11:29:03 +0100

    demo comments

--- liveViewTest_umbrella/apps/liveViewTest/lib/liveViewTest/application.ex
+++ liveViewTest_umbrella/apps/liveViewTest/lib/liveViewTest/application.ex
@@ -22,18 +22,21 @@ defmodule LiveViewTest.Application do
   end
 
   def connect_to_cluster do
-    # Docker internal DNS lookup
+    # DNS lookup
     IO.puts("Node name: #{node()}")
     {lookup_result, _} = System.cmd("nslookup", ["phoenix"])
 
-    Regex.scan(~r/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/, lookup_result)
+    # Parse IP out of nslookup result
+    Regex.scan(~r/\d+\.\d+\.\d+\.\d+/, lookup_result)
     |> List.flatten
+      # Filter out this container
     |> Enum.reject(
          fn x ->
-           {own_hostname, 0} = System.cmd("hostname", ["-i"])
-           x == own_hostname
+           {own_ip, 0} = System.cmd("hostname", ["-i"])
+           x == own_ip
          end
        )
+      # Connect to other node
     |> Enum.map(
          fn ip ->
            Node.connect(:"phoenix@#{ip}")
--- liveViewTest_umbrella/entrypoint.sh
+++ liveViewTest_umbrella/entrypoint.sh
@@ -1,6 +1,6 @@
 echo "Starting Up!"
 export CONTAINER_IP=$(hostname -i)
-echo "ip: $CONTAINER_IP , name will be: phoenix@{$CONTAINER_IP}"
+echo "ip: $CONTAINER_IP , name will be: phoenix@$CONTAINER_IP"
 export RELEASE_DISTRIBUTION=name
 export RELEASE_NODE="phoenix@${CONTAINER_IP}"
 export CONNECT_TO_SERVER=true
